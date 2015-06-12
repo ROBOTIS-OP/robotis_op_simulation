41b4846f211bd6d686600d16632838b8cf5eb19b
Kevin <kzweiev@gmail.com>
2015-06-12 17:27:04 +0900

    fixed enviornment in setup.py

--- robotis_op_gazebo/setup.py
+++ robotis_op_gazebo/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
-d['packages'] = ['darwin_gazebo']
+d['packages'] = ['robotis_op_gazebo']
 d['package_dir'] = {'': 'src'}
 
 setup(**d)
